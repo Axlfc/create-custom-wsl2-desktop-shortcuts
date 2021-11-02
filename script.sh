af0369ee15cea7c3b37defbea491d34275d594ef
Axlfc <axelfernandezcurros@gmail.com>
2021-11-02 13:55:40 +0100

    UPDATED: Identifying .ico files from commands

--- script.sh
+++ script.sh
@@ -5,9 +5,15 @@ declare ico_path="${wsl2desk}/git/icons/ico_images/"
 desk=$(pwd)
 if [ ${subsys} == "WSL2" ]; then
   echo "WSL2 Subsystem detected"
+
+  # We cannot launch nemo, because the folder it's not Nemo, it's Nemo_Desktop...
+  if [ -f /home/axl/Desktop/git/icons/ico_images/root/Nemo_Desktop ]; then
+    mv /home/axl/Desktop/git/icons/ico_images/root/Nemo_Desktop /home/axl/Desktop/git/icons/ico_images/root/Nemo
+  fi
+
   programs_list="$(cd /home/axl/Desktop; ls -p | grep -v /)"
   for program in ${programs_list[@]}^; do
-    name="$(echo ${program} | rev | cut -d "." -f2 | rev)"
+    name="$(echo ${program} | rev | cut -d "." -f2- | rev)"
     path="${desk}/${program}"
     COMM="$(echo "$(cat /home/axl/Desktop/*${name}.desktop | grep "Exec=")" | cut -d "=" -f2 | cut -d " " -f1 | uniq )"
     echo $COMM
@@ -22,14 +28,19 @@ if [ ${subsys} == "WSL2" ]; then
         cd "${rootfolder}"
         for folders in "$(ls)"; do
           #lowercase_root="${folders,,}"
-          for prog in ${folders[@]}; do
+          for prog_fold in ${folders[@]}; do
             COMM2="$(echo ${COMM} | tr '-' '_')"
-            if [ "${COMM2}" == "${prog,,}" ]; then
-              # We cannot launch nemo, because it's nemo-desktop, not nemo...
-              cd "$(pwd)/${prog}"
-              icon_path="$(pwd)/$(ls $(pwd))"
+            COMM3="$(echo "${prog_fold,,}" | cut -d "_" -f1- | tr '.' ' ')"
+            
+            if [ "${COMM2}" == "${COMM3}" ]; then
+
+              #echo "${COMM3}"
+              cd "$(pwd)/${prog_fold}"
+              #echo "${prog_fold}_icon.ico"
+              icon_path="$(echo "$(pwd)/$(echo ${prog_fold})")"
               
               echo "${COMM}_icon=${icon_path}"
+              cd ..
             else
               :
               #echo "error?"
@@ -47,7 +58,7 @@ if [ ${subsys} == "WSL2" ]; then
     
     #echo "Process correct name of CORRECT_PROGRAM_NAME_icon.ico image and save the image to safe path"
     #echo "Create Command file (.vbs) containing right information about the image path and command"
-    #echo "Create shortcut in windows' desktop (written in short.bat from custom-wsl2-desktop)"
+    #echo "Create shortcut in windows desktop (written in short.bat from custom-wsl2-desktop)"
   done
 else
   echo "We don't find any WSL2 Subsystem"
